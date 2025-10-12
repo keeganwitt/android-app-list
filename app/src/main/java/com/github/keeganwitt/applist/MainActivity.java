bfd45766d02beb45437a743e2ae61b6f9a35c8a0
Keegan Witt <keeganwitt@gmail.com>
2025-10-12 01:00:26 -0400

    style: Remove unused import

--- app/src/main/java/com/github/keeganwitt/applist/MainActivity.java
+++ app/src/main/java/com/github/keeganwitt/applist/MainActivity.java
@@ -19,7 +19,6 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
