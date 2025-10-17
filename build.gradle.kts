9b47974f267ede8aab34ead44b2a3b5aeb7c1f6d
Keegan Witt <keeganwitt@gmail.com>
2025-10-17 02:08:35 -0400

    build: Remove unneeded clean customization

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,3 @@ plugins {
     id("com.google.gms.google-services") version "4.4.4" apply false
     id("com.google.firebase.crashlytics") version "3.0.6" apply false
 }
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
