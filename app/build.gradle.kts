43468aee71dc08d90867c680aa37c8a914a4ae59
Keegan Witt <keeganwitt@gmail.com>
2026-01-02 15:51:26 -0500

    chore: Upgrade dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ android {
 
 dependencies {
     // Android dependencies
-    implementation("androidx.activity:activity-ktx:1.12.1")
+    implementation("androidx.activity:activity-ktx:1.12.2")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
