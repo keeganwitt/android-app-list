cb1614a117a40bad8f3ff6dc2dab13ba1eb76f4b
Keegan Witt <keeganwitt@gmail.com>
2026-09-02 20:30:19 -0400

    build: 1.16.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.github.keeganwitt.applist"
         minSdk = 24
         targetSdk = 37
-        versionCode = 32
-        versionName = "1.15.1"
+        versionCode = 33
+        versionName = "1.16.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
